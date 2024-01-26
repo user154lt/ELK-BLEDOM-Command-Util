8dc9c046f08b71709e8458c15c256c7c1172d501
User154lt <63184070+user154lt@users.noreply.github.com>
2024-01-26 14:24:09 +0000

    Tidying up

--- CommandUtils.kt
+++ CommandUtils.kt
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.Calendar
 
 class CommandUtils {
 
@@ -7,19 +7,17 @@ class CommandUtils {
     the boolean value is used twice when forming the command data.
      */
 
-    fun createOnOffCommand(isOn: Boolean): ByteArray {
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 4
-        commandData[2] = 4
-        commandData[3] = if (isOn) 1.toByte() else 0.toByte()
-        commandData[4] = 0
-        commandData[5] = if (isOn) 1.toByte() else 0.toByte()
-        commandData[6] = -1
-        commandData[7] = 0
-        commandData[8] = -17
-        return commandData
-    }
+    fun createOnOffCommand(isOn: Boolean): ByteArray = byteArrayOf(
+        0x7E.toByte(),
+        0x04.toByte(),
+        0x04.toByte(),
+        if (isOn) 1.toByte() else 0.toByte(),
+        0x00.toByte(),
+        if (isOn) 1.toByte() else 0.toByte(),
+        0xFF.toByte(),
+        0x00.toByte(),
+        0xEF.toByte()
+    )
 
     // This function is used to set the colour of the lights, the rgb values are in the range 0..255
 
@@ -27,131 +25,117 @@ class CommandUtils {
         redValue: Int,
         greenValue: Int,
         blueValue: Int
-    ): ByteArray {
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 7
-        commandData[2] = 5
-        commandData[3] = 3
-        commandData[4] = redValue.toByte()
-        commandData[5] = greenValue.toByte()
-        commandData[6] = blueValue.toByte()
-        commandData[7] = 16
-        commandData[8] = -17
-        return commandData
-    }
+    ): ByteArray = byteArrayOf(
+        0x7E.toByte(),
+        0x07.toByte(),
+        0x05.toByte(),
+        0x03.toByte(),
+        redValue.toByte(),
+        greenValue.toByte(),
+        blueValue.toByte(),
+        0x10.toByte(),
+        0xEF.toByte()
+    )
 
     /* This function creates command data to set the pattern data to one of the preset patterns.
     There are 29 available patterns which I will document separately, the numbering for the patterns
     starts at zero. This number is then added to 128 to create the data to send to the lights.
      */
 
-    fun createPatternCommand(pattern: Int): ByteArray {
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 5
-        commandData[2] = 3
-        commandData[3] = (pattern + 128).toByte()
-        commandData[4] = 3
-        commandData[5] = -1
-        commandData[6] = -1
-        commandData[7] = 0
-        commandData[8] = -17
-        return commandData
-    }
+    fun createPatternCommand(pattern: Int): ByteArray = byteArrayOf(
+        0x7E.toByte(),
+        0x05.toByte(),
+        0x03.toByte(),
+        (pattern.coerceIn(0..28) + 128).toByte(),
+        0x03,
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0x00.toByte(),
+        0xEF.toByte()
+    )
 
     /* This function creates the data to change the speed that the pattern will play at. The speed provided
     should be in the range 0..100
      */
 
-    fun createSpeedCommand(speed: Int): ByteArray {
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 4
-        commandData[2] = 2
-        commandData[3] = speed.toByte()
-        commandData[4] = -1
-        commandData[5] = -1
-        commandData[6] = -1
-        commandData[7] = 0
-        commandData[8] = -17
-        return commandData
-    }
+    fun createSpeedCommand(speed: Int): ByteArray = byteArrayOf(
+        0x7E.toByte(),
+        0x04.toByte(),
+        0x02.toByte(),
+        speed.coerceIn(0..100).toByte(),
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0x00.toByte(),
+        0xEF.toByte()
+    )
 
     /* This function creates the data to change the brightness of the lights. As with the speed function
     the value provided should be in the range 0..100
      */
 
-    fun createBrightnessCommand(brightness: Int): ByteArray {
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 4
-        commandData[2] = 1
-        commandData[3] = brightness.toByte()
-        commandData[4] = -1
-        commandData[5] = -1
-        commandData[6] = -1
-        commandData[7] = 0
-        commandData[8] = -17
-        return commandData
-    }
+    fun createBrightnessCommand(brightness: Int): ByteArray = byteArrayOf(
+        0x7E.toByte(),
+        0x04.toByte(),
+        0x01.toByte(),
+        brightness.coerceIn(0..100).toByte(),
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0x00.toByte(),
+        0xEF.toByte()
+    )
 
     /* This function creates the data to turn the light strip's microphone on or off, 1 is to turn on,
     0 is to turn off. After turning the mic on you must set an eq to make the lights begin to react to
     music/sounds
      */
 
-    fun createMicOnOffCommand(isOn: Boolean): ByteArray{
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 4
-        commandData[2] = 7
-        commandData[3] = if (isOn) 1 else 0
-        commandData[4] = -1
-        commandData[5] = -1
-        commandData[6] = -1
-        commandData[7] = 0
-        commandData[8] = -17
-        return commandData
-    }
+    fun createMicOnOffCommand(isOn: Boolean): ByteArray = byteArrayOf(
+        0x7E.toByte(),
+        0x04.toByte(),
+        0x07.toByte(),
+        if (isOn) 1 else 0,
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0x00.toByte(),
+        0xEF.toByte()
+    )
 
     /* This function creates the data to set the eq of the light strip's mic, you must set this
     after turning the mic on. eqMode is in the range 0..3, 0 = Classic, 1 = Soft, 2 = Dynamic,
     3 = Disco
     */
 
-    fun createMicEqCommand(eqMode: Int): ByteArray{
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 5
-        commandData[2] = 3
-        commandData[3] = (eqMode + 128).toByte()
-        commandData[4] = 4
-        commandData[5] = -1
-        commandData[6] = -1
-        commandData[7] = 0
-        commandData[8] = -17
-        return commandData
-    }
+    fun createMicEqCommand(eqMode: Int): ByteArray = byteArrayOf(
+        0x7E.toByte(),
+        0x05.toByte(),
+        0x03.toByte(),
+        (eqMode.coerceIn(0..3) + 128).toByte(),
+        0x04.toByte(),
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0x00.toByte(),
+        0xEF.toByte()
+    )
 
     /* This function creates the data to set the sensitivity of the light strip's mic. sensitivity
     can be in the range 0..100, for best results start with a lower sensitivity and turn up as
     required
      */
 
-    fun createMicSensitivityCommand(sensitivity: Int): ByteArray{
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 4
-        commandData[2] = 6
-        commandData[3] = sensitivity.toByte()
-        commandData[4] = -1
-        commandData[5] = -1
-        commandData[6] = -1
-        commandData[7] = 0
-        commandData[8] = -17
-        return commandData
-    }
+    fun createMicSensitivityCommand(sensitivity: Int): ByteArray = byteArrayOf(
+        0x7E.toByte(),
+        0x04.toByte(),
+        0x06.toByte(),
+        sensitivity.coerceIn(0..100).toByte(),
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0xFF.toByte(),
+        0x00.toByte(),
+        0xEF.toByte()
+    )
 
 
     /* This function creates the data to set the internal clock of the light strip, in its current state
@@ -161,18 +145,18 @@ class CommandUtils {
      */
 
     fun createSyncTimeCommand(): ByteArray {
-        val commandData = ByteArray(9)
         val calendar = Calendar.getInstance()
-        commandData[0] = 126
-        commandData[1] = 7
-        commandData[2] = -125
-        commandData[3] = calendar.get(Calendar.HOUR_OF_DAY).toByte()
-        commandData[4] = calendar.get(Calendar.MINUTE).toByte()
-        commandData[5] = calendar.get(Calendar.SECOND).toByte()
-        commandData[6] = (calendar.get(Calendar.DAY_OF_WEEK) - 1).toByte()
-        commandData[7] = -1
-        commandData[8] = -17
-        return commandData
+        return byteArrayOf(
+            0x7E.toByte(),
+            0x07.toByte(),
+            0x83.toByte(),
+            calendar.get(Calendar.HOUR_OF_DAY).toByte(),
+            calendar.get(Calendar.MINUTE).toByte(),
+            calendar.get(Calendar.SECOND).toByte(),
+            (calendar.get(Calendar.DAY_OF_WEEK) - 1).toByte(),
+            0xFF.toByte(),
+            0xEF.toByte()
+        )
     }
 
     /* This function is to create data that can control the scheduling of the lights on and off times,
@@ -195,31 +179,31 @@ class CommandUtils {
         isOn: Boolean, //True to set an ON time, false to set an OFF time
         isSet: Boolean //True to SET an on/off time, false to CLEAR an on/off time
     ): ByteArray {
-        val setOrClearFlag = if (isSet) 128 else 0
-        val weekdaysInt = createWeekdaysInt(weekdays = weekdays)
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 8
-        commandData[2] = -126
-        commandData[3] = hour.toByte()
-        commandData[4] = minute.toByte()
-        commandData[5] = second.toByte()
-        commandData[6] = if (isOn) 0 else 1 //Timing set bit, schedule on = 0 schedule off = 1 (Yes really!)
-        commandData[7] = (setOrClearFlag or weekdaysInt).toByte() // 128 or weekDays int to set, 0 or weekDays int to clear
-        commandData[8] = -17
-        return commandData
+        val setOrClearMask = if (isSet) 128 else 0
+        val packedWeekdays = packWeekdays(weekdays = weekdays)
+        return byteArrayOf(
+            0x7E.toByte(),
+            0x08.toByte(),
+            0x82.toByte(),
+            hour.toByte(),
+            minute.toByte(),
+            second.toByte(),
+            if (isOn) 0x00.toByte() else 0x01.toByte(),
+            (setOrClearMask or packedWeekdays).toByte(),
+            0xEF.toByte()
+        )
     }
 
 
-    private fun createWeekdaysInt(weekdays: List<Boolean>): Int{
-        var weekdaysInt = 0
-        for (i in 0..6){
-            if (weekdays[i]){
+    private fun packWeekdays(weekdays: List<Boolean>): Int {
+        var packed = 0
+        for (i in 0..6) {
+            if (weekdays[i]) {
                 val mask: Int = 1 shl i
-                weekdaysInt = weekdaysInt or mask
+                packed = packed or mask
             }
         }
-        return weekdaysInt
+        return packed
     }
 
     /* This should be the data required to change rgb order of the wires, not really for general use but potentially 
@@ -236,20 +220,18 @@ class CommandUtils {
     After using this command you must turn the controller off and back on again for changes to take effect.
     */
     fun createOrderChangeCommand(
-            firstWire: Int,
-            secondWire: Int,
-            thirdWire: Int
-    ): ByteArray{
-        val commandData = ByteArray(9)
-        commandData[0] = 126
-        commandData[1] = 6
-        commandData[2] = -127
-        commandData[3] = firstWire.toByte()
-        commandData[4] = secondWire.toByte()
-        commandData[5] = thirdWire.toByte()
-        commandData[6] = -1
-        commandData[7] = 0
-        commandData[8] = -17
-        return commandData
-    }
+        firstWire: Int,
+        secondWire: Int,
+        thirdWire: Int
+    ): ByteArray = byteArrayOf(
+        0x7E.toByte(),
+        0x06.toByte(),
+        0x81.toByte(),
+        firstWire.toByte(),
+        secondWire.toByte(),
+        thirdWire.toByte(),
+        0xFF.toByte(),
+        0x00.toByte(),
+        0xEF.toByte()
+    )
 }
